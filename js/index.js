07cffd7146b1150adbfa4f962072d561c6079994
Aliaksei Karzhou <alexius90e@gmail.com>
2024-07-10 16:35:22 +0300

    feat: added ad-banners

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 import './menu.js';
 import './custom-select.js';
+
+const adBannersSwiper = new Swiper('.ad-banners .swiper', {
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.ad-banners .ad-banners__slider-controls-next',
+    prevEl: '.ad-banners .ad-banners__slider-controls-prev',
+  },
+});
