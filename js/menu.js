92ac7e3fe9fbc5fc9ea98bf62e8219d3937a8070
Aliaksei Karzhou <alexius90e@gmail.com>
2024-07-10 15:15:35 +0300

    feat: added main-nav click listener

--- js/menu.js
+++ js/menu.js
@@ -6,4 +6,15 @@ if (burgerButton && mainNav) {
     event.currentTarget.classList.toggle('active');
     mainNav.classList.toggle('active');
   });
+
+  mainNav.addEventListener('click', (event) => {
+    const isNavLink = event.target.classList.contains('main-nav__nav-item-link');
+    const isContactsLink = event.target.classList.contains('main-nav__contacts-link');
+    const isInfoLink = event.target.classList.contains('main-nav__info-text-link');
+
+    if (isNavLink || isContactsLink || isInfoLink) {
+      event.currentTarget.classList.remove('active');
+      burgerButton.classList.remove('active');
+    }
+  });
 }
